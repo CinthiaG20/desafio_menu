d9510eeaeac5e440bf3f006adcb2a948fbf8e32f
FragaEmanuel <emanuel.fraga@correo.ucu.edu.uy>
2024-08-21 08:58:37 -0300

    implementacion de la clase Dish

--- src/Dish.cs
+++ src/Dish.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 namespace Ucu.Poo.Restaurant;
 
 /// <summary>
@@ -5,5 +7,31 @@ namespace Ucu.Poo.Restaurant;
 /// </summary>
 public class Dish
 {
-    
+    private string name;                //Atributo nombre
+    public string Name
+    {
+        get { return name; }
+        set { name = value ; }
+    }
+
+    private int price;                  //Atributo precio
+    public int Price
+    {
+        get { return price; }
+        set { price = value ; }
+    }
+
+    private bool isVegetarian;         //Atributo "vegetariano?"
+    public bool IsVegetarian
+    {
+        get { return isVegetarian; }
+        set { isVegetarian = value; }
+    }
+
+    public Dish(string name, int price, bool isVegetarian)          //Metodo Constructor
+    {
+        this.Name = name;
+        this.Price = price;
+        this.IsVegetarian = isVegetarian;
+    }
 }
